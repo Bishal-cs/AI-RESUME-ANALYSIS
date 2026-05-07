5adbd213d208cf8891e7a10d7930b14c3ff61f24
Bishal-cs <bishaldas1152006@gmail.com>
2026-05-07 12:51:34 +0530

    Remove privacy key

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import dotenv
+import os
 
-DATABASE_URL = "mysql+mysqldb://tEYfmUh6aFwMfdv.root:jju2ImUNodnpORbg@gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/test?ssl_mode=VERIFY_IDENTITY&ssl_ca=<CA_PATH>"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
